1fa7a97ed3ed395a42ab7d00ea6ed2ce55a9db49
claudio contardi <claudio_pros@hotmail.com>
2022-06-05 23:34:15 -0300

    primer commit

---
+++ js/proyectoFinalContardi.js
@@ -0,0 +1,51 @@
+
+
+//********************************PROYECTO FINAL EN CRECIMIENTO*********************************** */
+
+class Prestamo {
+    constructor(nombre, apellido, monto, cuotas) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.monto = monto
+        this.cuotas = cuotas
+        this.intereses = 1.81
+         this.getFactorCrediticio = () => { 
+             switch (this.cuotas) {
+                 case 1:
+                     return parseInt(this.cuotas)/ 1
+                 case 3:
+                     return parseInt(this.cuotas)/ 3
+                 case 6:
+                     return parseInt(this.cuotas)/ 6
+                 case 12: 
+                     return parseInt(this.cuotas)/ 12
+                 default: 
+                     return 0
+             }
+         }
+        this.prestamoAprobado = () => { 
+            const factorCrediticio = this.getFactorCrediticio()
+            const resultado = (this.monto * this.intereses) / this.cuotas 
+                return parseFloat(resultado.toFixed(2))
+        }
+        
+    }
+}
+
+//! variables 
+let monto = 0
+let cuotas = 0
+
+//!app.js
+function nuevoPrestamo() {
+    debugger
+    nombre = prompt("ingrese su nombre")
+    apellido = prompt ("ingrese su apellido")
+    monto = parseInt(prompt("Ingresa el monto a solicitar:"))
+    cuotas = parseInt(prompt("¿que cuota prefieres? 1/ 3/ 6/ 12"))
+    
+    const prestamoSolcitado = new Prestamo(nombre, apellido, monto, cuotas)
+    const prestamoAprobado = prestamoSolcitado.prestamoAprobado()
+    console.log("Valor de cuota a pagar, es: ARS", prestamoAprobado)
+}
+
