d6fe2cdf69bafc7a85bfcc92bfd26f87cfa90695
claudio contardi <claudio_pros@hotmail.com>
2022-06-20 22:48:24 -0300

    trabajar con DOM

---
+++ js/variables.js
@@ -0,0 +1,22 @@
+let inputNombre = "ingrese nombre";
+let inputApellido = "ingrese apellido"
+let inputMonto = 0
+let inputCuota = 0
+let interes = 1.45
+
+
+const titulo = document.getElementById("titulo")
+const tituloDos = document.getElementById("tituloDos")
+const resultadoSimulador = document.getElementById("resultadoSimulador")
+const botonSimulador = document.getElementById("botonSimulador")
+
+
+const formulario = document.getElementById("formulario")
+
+const inforNombre = document.getElementById("inputNombre")  //**para usar con la funcion VALUE (INFORMACION DE DATOS INGRESADOS) */
+const inforApellido = document.getElementById("inputApellido") //**para usar con la funcion VALUE (INFORMACION DE DATOS INGRESADOS) */
+const inforMonto = document.getElementById("inputMonto") //**para usar con la funcion VALUE (INFORMACION DE DATOS INGRESADOS) */
+const inforCuota = document.getElementById("inputCuota") //**para usar con la funcion VALUE (INFORMACION DE DATOS INGRESADOS) */
+
+
+
